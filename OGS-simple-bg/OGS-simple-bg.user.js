eaf03a7deed61a6ddb2a2a9b6116c501b01cff89
Claudiu Papasteri <claudiu.papasteri@gmail.com>
2025-08-27 22:11:27 +0300

    Added OGS-simple-bg

---
+++ OGS-simple-bg/OGS-simple-bg.user.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         OGS Simple Background
+// @version      1.0
+// @description  Just set a custom background on OGS game/review/demo pages
+// @credits      This is barebones version of a script by SoumyaK4 (https://soumyak4.in): https://github.com/SoumyaK4/OGS-Tampermonkey
+// @match        https://online-go.com/game/*
+// @match        https://online-go.com/review/*
+// @match        https://online-go.com/demo/*
+// @downloadURL  https://raw.githubusercontent.com/ClaudiuPapasteri/Go-themes/OGS-simple-bg/OGS-simple-bg.user.js
+// @updateURL    https://raw.githubusercontent.com/ClaudiuPapasteri/Go-themes/OGS-simple-bg/OGS-simple-bg.user.js
+// @license      MIT
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  const DEFAULT_BG = 'https://claudiupapasteri.github.io/Go-themes/OGS-simple-bg/Immortals_Playing_Weiqi_on_Penglai_(overley).jpg';
+
+  function setBackground() {
+    document.documentElement.style.backgroundImage = `url('${DEFAULT_BG}')`;
+    document.documentElement.style.backgroundSize = 'cover';
+    document.documentElement.style.backgroundPosition = 'center';
+    document.documentElement.style.backgroundRepeat = 'no-repeat';
+
+    // also clear the game container background if it exists
+    const container = document.getElementById('default-variant-container');
+    if (container) {
+      container.style.backgroundImage = `url('${DEFAULT_BG}')`;
+      container.style.backgroundSize = 'cover';
+      container.style.backgroundPosition = 'center';
+      container.style.backgroundRepeat = 'no-repeat';
+      container.style.backgroundColor = 'transparent';
+    }
+  }
+
+  // Run once on page load
+  window.addEventListener('load', setBackground);
+
+  // Run again if URL changes (for navigation within OGS SPA)
+  new MutationObserver(setBackground).observe(document, { childList: true, subtree: true });
+})();
